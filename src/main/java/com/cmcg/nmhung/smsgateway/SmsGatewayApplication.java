7edb8fe3641a0fc5a9110c40c5535796f7d643b7
nmhung@cmc.com.vn <nmhung@cmc.com.vn>
2021-05-04 10:52:18 +0700

    apearrance in áºá»eureka server

--- src/main/java/com/cmcg/nmhung/smsgateway/SmsGatewayApplication.java
+++ src/main/java/com/cmcg/nmhung/smsgateway/SmsGatewayApplication.java
@@ -2,8 +2,12 @@ package com.cmcg.nmhung.smsgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@FeignClient
 public class SmsGatewayApplication {
 
     public static void main(String[] args) {
